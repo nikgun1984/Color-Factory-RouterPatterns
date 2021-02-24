0d242b31d38ba0c021a1542c8c5f26eb301829d6
Nick Gundobin <ngundobin@gmail.com>
2021-02-23 21:24:22 -0500

    tests:smoke/snapshot/specialized

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,62 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, fireEvent } from "@testing-library/react";
+import App from "./App";
+import { MemoryRouter } from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//smoke test
+it("mounts without crashing", function () {
+	const { getByText } = render(
+		<MemoryRouter initialEntries={["/colors/"]}>
+			<App />
+		</MemoryRouter>
+	);
+	const redLink = getByText("red");
+	expect(redLink).toBeInTheDocument();
+});
+
+//snapshot tests
+it("matches snapshot", function () {
+	const { asFragment } = render(<App />);
+	expect(asFragment()).toMatchSnapshot();
+});
+
+it("navbar links like red", function () {
+	const { getByText } = render(
+		<MemoryRouter initialEntries={["/colors/"]}>
+			<App />
+		</MemoryRouter>
+	);
+
+	const link = getByText("red");
+	fireEvent.click(link);
+	expect(getByText("ISN'T IT BEAUTIFUL")).toBeInTheDocument();
+
+	const backLink = getByText("GO BACK HOME");
+	fireEvent.click(backLink);
+	expect(getByText("Welcome to the color factory.")).toBeInTheDocument();
+});
+
+it("checks if form submits", function () {
+	const { getByText, getByLabelText, queryByText } = render(
+		<MemoryRouter initialEntries={["/colors/new"]}>
+			<App />
+		</MemoryRouter>
+	);
+	const addColorLink = getByText("Add a Color");
+	expect(addColorLink).toBeInTheDocument();
+
+	fireEvent.click(addColorLink);
+
+	// no items yet
+	expect(queryByText("pink")).not.toBeInTheDocument();
+
+	//fill out the form
+	const textInput = getByLabelText("Name Color");
+	const colorInput = getByLabelText("Color");
+	const submitBtn = queryByText("Add this Color");
+
+	fireEvent.change(textInput, { target: { value: "pink" } });
+	fireEvent.change(colorInput, { target: { value: "#FF00FF" } });
+	fireEvent.click(submitBtn);
+
+	expect(queryByText("pink")).toBeInTheDocument();
 });
